9cf597214adae227202d7b345979f57752b32a6f
Pranavo <158192327+PRANAVO02@users.noreply.github.com>
2025-09-19 00:08:12 +0530

    Delete CloudEncryption directory

--- CloudEncryption/utils/hash_utils.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-def hash_fragment(fragment_path):
-    sha = hashlib.sha256()
-    with open(fragment_path, "rb") as f:
-        while chunk := f.read(8192):
-            sha.update(chunk)
-    return sha.hexdigest()
